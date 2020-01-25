49bcd7f3744063faaca00c7ca31be5ea6914718b
Andres Gutierrez <agutierrez.er@gmail.com>
2020-01-25 16:30:33 +0100

    Use Layout on Index Page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,13 @@
-import React from "react"
+import React from "react";
 
-export default () => <div>Hello world!</div>
+import Layout from "../components/layout";
+
+const IndexPage = () => {
+  return (
+    <Layout>
+      This is the homepage
+    </Layout>
+  ) 
+}
+
+export default IndexPage;
