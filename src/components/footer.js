afaed34d1dd94255875896dcbfd560ec9d2ec34c
Andres Gutierrez <agutierrez.er@gmail.com>
2020-01-26 00:11:50 +0100

    Add styles to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,20 +1,22 @@
 import React from "react"
 
-import { socialMedia, siteAuthor } from '@config';
+import { socialMedia, name } from '@config';
+
+import classes from './footer.module.scss';
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className={classes.footer}>
       <div>
         <div>
           <span>Diseñado y Desarrollado por</span>
         </div>
         <div>
-          <span>{siteAuthor}</span>
+          <span>{name}</span>
           <span> 2020</span>
         </div>
       </div>
-      <div>
+      <div className={classes.links}>
         {
           socialMedia.map(({name, url}, i) => (
             <a key={i} href={url}>
