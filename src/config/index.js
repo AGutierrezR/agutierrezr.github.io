22c2f69c0046ca15b5553130a1327e76c72dbdd6
Andres Gutierrez <agutierrez.er@gmail.com>
2020-01-25 19:11:49 +0100

    Create config file
    This contain some data, like:
    - name
    - email
    - social media urls

---
+++ src/config/index.js
@@ -0,0 +1,25 @@
+module.exports = {
+  siteTitle: 'Andres Gutierrez | Desarrollador Front End',
+  siteURL: 'https://agutierrezr.github.io',
+  siteDescription: '',
+  name: 'Andres Gutierrez',
+  email: 'agutierrez.codes@gmail.com',
+  socialMedia: [
+    {
+      name: 'Email',
+      url: '#'
+    },
+    {
+      name: 'Github',
+      url: '#'
+    },
+    {
+      name: 'LinkedIn',
+      url: '#'
+    },
+    {
+      name: 'Instagram',
+      url: '#'
+    }
+  ]
+}
