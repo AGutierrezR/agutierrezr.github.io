95950e7c996e1cd3b002fee2be811a6aeaca4e42
Andres Gutierrez <agutierrez.er@gmail.com>
2020-01-28 12:55:40 +0100

    Add more icons

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,12 +1,23 @@
-import IconInstagram from './instagram';
-import IconEnv from './env';
-import IconGithub from './github';
-import IconLinkedin from './linkedin';
-
+import IconInstagram from "./instagram"
+import IconEnv from "./env"
+import IconGithub from "./github"
+import IconLinkedin from "./linkedin"
+import IconHome from "./home"
+import IconPencil from "./pencil"
+import IconSuitcase from "./suitcase"
+import IconFile from "./file"
+import IconUser from "./user"
+import IconLogo from "./logo"
 
 export {
   IconInstagram,
   IconEnv,
   IconGithub,
-  IconLinkedin
+  IconLinkedin,
+  IconHome,
+  IconPencil,
+  IconSuitcase,
+  IconFile,
+  IconUser,
+  IconLogo
 }
