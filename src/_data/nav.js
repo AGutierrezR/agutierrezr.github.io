d13c9571922b3cf50717a8e6fa8021bf57b78e2b
Andres Gutierrez <agutierrez.codes@gmail.com>
2023-06-10 23:21:35 +0200

    Improve Header and Footer

---
+++ src/_data/nav.js
@@ -0,0 +1,20 @@
+const navItems = [
+  {
+    name: 'Inicio',
+    path: '/',
+  },
+  // {
+  //   name: 'Blog',
+  //   path: '/blog',
+  // },
+  // {
+  //   name: 'Sobre mi',
+  //   path: '/about',
+  // },
+  // {
+  //   name: 'Links',
+  //   path: '/links',
+  // },
+]
+
+export default navItems
