043cae4d21e2d695e6acbcadfe6a0dadf4ddf1e2
Andres Gutierrez <agutierrez.codes@gmail.com>
2023-06-11 01:08:15 +0200

    Install MDX Support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
-import tailwind from '@astrojs/tailwind'
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      config: { applyBaseStyles: false },
-    }),
-  ],
-})
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), mdx()]
+});
