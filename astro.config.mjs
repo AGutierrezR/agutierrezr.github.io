a949d71bde1cd93cc32914c506c117700f67cdb7
Andres Gutierrez <agutierrez.codes@gmail.com>
2023-06-12 19:13:10 +0200

    Add support for yaml files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,20 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
 
-import mdx from "@astrojs/mdx";
+import mdx from '@astrojs/mdx'
+import yaml from '@rollup/plugin-yaml'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    config: {
-      applyBaseStyles: false
-    }
-  }), mdx()]
-});
+  vite: {
+    plugins: [yaml()],
+  },
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    mdx(),
+  ],
+})
