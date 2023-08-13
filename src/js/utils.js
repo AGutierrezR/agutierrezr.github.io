161d220dbdc204ab97a4735f15e1bee48f5ccf0d
Andres Gutierrez <agutierrez.codes@gmail.com>
2023-08-14 01:59:46 +0200

    Add post format features

--- src/js/utils.js
+++ src/js/utils.js
@@ -2,3 +2,34 @@ export function formatDate(date) {
   return new Date(date).toLocaleDateString(undefined, { timeZone: 'UTC' })
 }
 
+export function formatBlogPosts(posts, {
+  filterOutDrafts = true,
+  filterOutFuturePosts = true,
+  sortByDate = true,
+  limit = undefined,
+} = {}) {
+
+  const filterdedPost = posts.reduce((acc, post) => {
+    const { date, draft } = post.frontmatter;
+
+    // Filter out draft posts
+    if (filterOutDrafts && draft) return acc;
+
+    // Filter out future posts
+    if (filterOutFuturePosts && new Date(date) > new Date()) return acc
+
+    acc.push(post)
+
+    return acc
+  }, [])
+
+  if (sortByDate) {
+    filterdedPost.sort((a, b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date))
+  }
+
+  if (typeof limit === 'number') {
+    return filterdedPost.slice(0, limit)
+  }
+
+  return filterdedPost
+}
